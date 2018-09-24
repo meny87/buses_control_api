95f3206b052811a7b8733b0d2f98ce85f8641afc
Emmanuel Casas <emmanuelcasas@MacBook-Pro-de-Emmanuel.local>
2018-09-24 16:13:51 -0500

    Adding unique attribute to cuotas index

--- server/models/cuota.js
+++ server/models/cuota.js
@@ -30,7 +30,7 @@ var CuotaSchema = new mongoose.Schema({
   }
 });
 
-CuotaSchema.index({unidad:1, periodo:1});
+CuotaSchema.index({unidad:1, periodo:1}, {unique: true});
 
 var Cuota = mongoose.model('Cuota', CuotaSchema);
 module.exports = {Cuota};
